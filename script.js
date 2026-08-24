d65bc7dbd0cd081f11a0dfe8e686ab22d6aa8421
Kartikeya Mishra <Kartikeya9127@outlook.com>
2026-08-24 23:00:58 +0530

    completed the to do list. moving on to pomodoro

--- script.js
+++ script.js
@@ -4,43 +4,66 @@ const taskList = document.getElementById("task-list");
 
 inputButton.addEventListener("click",addTask);
 
+
+
+
 function addTask(){
+
+
+
     if(inputBox.value.trim() !== ""){
     const taskText = inputBox.value.trim();
 
+
     const task = document.createElement("li");
+
     const taskTextElement = document.createElement("span");
     taskTextElement.textContent = taskText;
+
+
     task.appendChild(taskTextElement);
     taskList.appendChild(task);
-    task.addEventListener("click", completeTask)
-  
+
+    taskTextElement.addEventListener("click", completeTask)
+    inputBox.addEventListener("keydown")
+
     inputBox.value = "";
+
     const deleteButton = document.createElement("button");
-    deleteButton.textContent = "x"
+    deleteButton.textContent = "x";
+
+ 
+
     deleteButton.classList.add("deleteButton");
+
+    
     task.appendChild(deleteButton);
     deleteButton.addEventListener("click", deleteTask);
 
     }
-}
+
+} 
+
 
 
 function deleteTask(event){
+
     const li = event.target.parentElement;
 
     taskList.removeChild(li);
 
 }
+
+
 function completeTask(e){
-    e.target.classList.toggle("completed")
+
+    
+
+    
+        e.target.classList.toggle("completed")
+
+
    
-    
-    
-    
-    
-
-
 }
 
 
